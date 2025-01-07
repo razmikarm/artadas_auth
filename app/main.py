d89245c181e65271501dbfc3a6e72ba7d01f2d57
Razmik Poghosyan <razmikarm@gmail.com>
2025-01-07 05:34:22 +0400

    Commented apply_migrations untill fixing logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.utils.migrations import apply_migrations
+
+# from app.utils.migrations import apply_migrations
 from app.routers import auth, users
 from app.core.config import settings
 
@@ -8,7 +9,7 @@ from app.core.config import settings
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Perform any startup logic here
-    apply_migrations()  # Apply migrations when the app starts
+    # apply_migrations()  # Apply migrations when the app starts
     yield  # Control returns to the application during runtime
     # Perform any shutdown logic here if needed
 
